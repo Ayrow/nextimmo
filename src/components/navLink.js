344fc4f9961a5e73fdba2d94c90b129cf7bc9bbc
Aymeric <aymeric.pilaert@hotmail.fr>
2023-04-24 17:25:51 +0200

    moved the navlink to separate component and changed color of link if active

--- src/components/navLink.js
+++ src/components/navLink.js
@@ -1,7 +1,18 @@
-import React from 'react';
+'use client';
+import Link from 'next/link';
+import { useSelectedLayoutSegment } from 'next/navigation';
 
-const navLink = () => {
-  return <div>navLink</div>;
+const NavLink = ({ label, path, targetSegment }) => {
+  const activeSegment = useSelectedLayoutSegment();
+  return (
+    <Link
+      className={`${
+        activeSegment === targetSegment ? 'text-blue-500' : 'text-white'
+      } block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0`}
+      href={path}>
+      {label}
+    </Link>
+  );
 };
 
-export default navLink;
+export default NavLink;
