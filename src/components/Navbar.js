f295dc1193a178e136ae351b14db70040d353bc3
Aymeric <aymeric.pilaert@hotmail.fr>
2023-05-02 16:08:51 +0200

    adapted the navbar to look better on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,27 +21,11 @@ const Navbar = () => {
         </Link>
         <div className='flex md:order-2'>
           <AccountButton />
-          <button
-            type='button'
-            className='inline-flex items-center p-2 text-sm  rounded-lg md:hidden  focus:outline-none focus:ring-2  text-gray-400 hover:bg-gray-700 focus:ring-gray-600'>
-            <span className='sr-only'>Open main menu</span>
-            <svg
-              className='w-6 h-6'
-              aria-hidden='true'
-              fill='currentColor'
-              viewBox='0 0 20 20'
-              xmlns='http://www.w3.org/2000/svg'>
-              <path
-                fillRule='evenodd'
-                d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-                clipRule='evenodd'></path>
-            </svg>
-          </button>
         </div>
         <div className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'>
           <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border  rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  bg-gray-800 md:bg-gray-900 border-gray-700'>
             <li>
-              <NavLink path='/' label='Accueil' targetSegment={null} />
+              <NavLink path='/' label='Accueil' targetSegment='/' />
             </li>
             <li>
               <NavLink
@@ -59,6 +43,11 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+      </div>
+      <div className='bg-sky-950 md:hidden flex gap-5 justify-center'>
+        <NavLink path='/' label='Accueil' targetSegment='/' />
+        <NavLink path='/listings' label='Listings' targetSegment='/listings' />
+        <NavLink path='/contact' label='Contact' targetSegment='/contact' />
       </div>
     </nav>
   );
